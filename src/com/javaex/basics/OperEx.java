dd2691d0f456e531ef14143bf5e814d726d16daf
princessHan <princessHan@DESKTOP-9PGKR9M>
2021-05-03 11:55:10 +0900

    논리연산자, 비트 연산자

--- src/com/javaex/basics/OperEx.java
+++ src/com/javaex/basics/OperEx.java
@@ -4,7 +4,57 @@ public class OperEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		arithOperEx();
+		//arithOperEx();
+		//logicOper();
+		bitOper();
+	}
+	
+	//비트연산자
+	//비트단위로 미세한 조작이 필요할 때 사용
+	//하드웨어 제어, 이미지 프로세싱
+	private static void bitOper() {
+		byte b1=0b1101;
+		byte b2=0b1111;
+		
+		System.out.println("b1"+Integer.toBinaryString(b1));
+		System.out.println("b1"+Integer.toBinaryString(b2));
+		int result= b1 & b2;//비트 논리 곱 :둘다 1이어야 1비트로 세팅
+		System.out.println("b1&b2:"+Integer.toBinaryString(result));
+		result=b1 |b2;
+		System.out.println("b1|b2:"+Integer.toBinaryString(result));
+		result=~b1;//비트 논리 부정 :1<->0
+		System.out.println("~b1:"+Integer.toBinaryString(result));
+	}
+	//비교연산,논리연산
+	//결과로boolean반환, 논리값으로 프로그램의 흐름제어->중요
+	private static void logicOper() {
+		int n1 =7;
+		int n2=3;
+		
+		System.out.println("a와 b가 같은가?"+(n1==n2));
+		System.out.println("a와 b가 같지않은가?"+(n1!=n2));
+		
+		//논리 연산
+		//집합을 떠올리자
+		
+		int n3=5;
+		
+		boolean r1=n3>0;
+		boolean r2=n3<10;
+		boolean r1andr2=r1&&r2;
+		System.out.println("n3가 0초과 10미만 영역에 있는가?"+r1andr2);
+		
+		r1=n3<=0;
+		r2=n3>=10;
+		boolean r1orr2=r1||r2;
+		System.out.println("n3가 0이하거나 n3가 10이상의 영역?"+r1orr2);
+		
+		boolean rNot= n3>0&&n3<10;//여집합 영역
+		System.out.println(rNot);
+		
+		
+		
+		
 	}
 	private static void arithOperEx() {
 		int a =7;
@@ -47,6 +97,7 @@ public class OperEx {
 		//NaN가 포함된 연산식->NaN:연산 불가의 의미
 		System.out.println("0.0/0.0 is NaN?"+Double.isNaN(0.0/0.0));
 		System.out.println(0.0/0.0+10);
+		
 		
 	}
 }
